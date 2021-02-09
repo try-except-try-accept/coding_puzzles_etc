2e5b54e8e13a6dfb900c8e726a86bf16cc35d036
Christopher Hall <CHall@bkkprep.ac.th>
2021-02-09 15:11:34 +0700

    initial

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # coding_puzzles_etc
  Solutions to logic puzzles etc.
+
+
+## optimal_path_through_triangle.py
+
+What's the best path through the triangle / weighted graph thing? Probably not to try every path. But let's do that anyway.
---
+++ optimal_path_through_triangle.py
@@ -0,0 +1,66 @@
+from random import randint
+
+HEIGHT = 5#randint(3, 10)
+
+def random_triangle():
+     triangle = ''
+
+     
+     buffer = (HEIGHT+1//2)     
+     for i in range(HEIGHT):       
+          triangle += buffer * " "
+          buffer -= 1
+          for j in range(i+1):
+               n = randint(0, 9)
+               triangle += str(n) + " "
+          triangle += "\n"                   
+          
+     return triangle
+
+test_data = random_triangle()
+
+print("Testing ... ")
+print(test_data)
+
+triangle = [row.strip().split() for row in test_data.strip().split("\n")]
+
+depth = HEIGHT - 1
+
+possible_paths = [bin(num)[2:].zfill(depth) for num in range(0, 2**depth)]
+
+
+traversals = [[]]
+
+
+attempts = 0
+best = [0]
+
+for path in possible_paths:
+     traversals.append([int(triangle[0][0])]) # start from top of triangle
+     current = 0
+     level = 1          
+     for turn in path:
+          
+          if int(turn):
+               current += 1
+
+          traversals[-1].append(int(triangle[level][current]))
+          level += 1
+
+     if sum(traversals[-1]) > sum(best):
+          best = traversals[-1]
+
+     attempts += 1
+    
+
+print("Best path is: ", best, sum(best))
+     
+
+
+
+
+
+          
+
+          
+     
